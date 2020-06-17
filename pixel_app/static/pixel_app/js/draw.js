7b307769c5f0c61516cc7faa5f4d559d6adc239b
Pete Jones <pwj2012@gmail.com>
2020-06-16 19:36:48 -0700

    tested stuff

--- pixel_app/static/pixel_app/js/draw.js
+++ pixel_app/static/pixel_app/js/draw.js
@@ -84,6 +84,62 @@ var app = new Vue({
                 this.ctx.lineTo(this.w, y);
                 this.ctx.stroke();
             };
+
+            // // drawing a circle for practice
+            // for(let i=1; i<8; i++) {
+            //     this.ctx.beginPath();
+            //     this.ctx.arc(250, 250, 250*(i/7), 0, 2*Math.PI);
+            //     this.ctx.stroke();
+            // };
+
+            // // rainbow here
+            // for(let i=0; i<7; i++) {
+            //     console.log(i)
+            //     this.ctx.beginPath();
+            //     this.ctx.arc(250, 625, 500 - i * 20, 0, 2*Math.PI);
+            //     this.ctx.stroke();
+            // };
+
+            // // mountain landscape
+            // let mountArr = [[125, 195], [375, 195]]
+            // for (let i=0; i<mountArr.length; i++) {
+            //     let x = mountArr[i][0];
+            //     let y = mountArr[i][1];
+            //     // console.log(mountArr[i])
+            //     // console.log('mountain', x, y)
+            //     this.ctx.beginPath();
+            //     this.ctx.moveTo(x, y);
+            //     this.ctx.lineTo(x - Math.abs(y - this.h), this.h);
+            //     this.ctx.lineTo(x + Math.abs(y - this.h), this.h);
+            //     this.ctx.closePath();
+            //     this.ctx.stroke();
+
+            //     this.ctx.beginPath();
+            //     this.ctx.moveTo(x - 50, y + 50);
+            //     this.ctx.lineTo(x + 50, y + 50);
+            //     this.ctx.stroke();
+
+            // }
+
+            // this.ctx.beginPath();
+            // this.ctx.arc(275, 225, 75, 0, 2*Math.PI);
+            // this.ctx.stroke();
+
+            // for (let i=0; i<100; i++) {
+            //     let x = Math.random()*this.w
+            //     let y = Math.random()*320
+            //     this.ctx.beginPath();
+            //     this.ctx.arc(x, y, 5, 0, 2*Math.PI);
+            //     this.ctx.stroke();
+            // }
+
+            // // rainbow arc
+            // for (let i=0; i<8; i++) {
+            //     this.ctx.beginPath();
+            //     this.ctx.arc(500, 500, 500 - i*20, 0, 2*Math.PI);
+            //     this.ctx.stroke();
+            // }
+
         },
 
         testFunc: function(color) {
@@ -103,20 +159,20 @@ var app = new Vue({
             if (this.drawing === true) {
                 // console.log(this.gridArr)
                 let inc = parseInt(this.increment)
-                console.log(event)
+                // console.log(event)
                 let x = event.offsetX;
                 let y = event.offsetY;
-                console.log(x, y)
+                // console.log(x, y)
                 for (let i=0; i<this.gridArr.length; i++) {
                     // console.log(i)
                     let pixel = this.gridArr[i];
-                    console.log(pixel)
+                    // console.log(pixel)
                     if (x >= pixel.x
                         && x < pixel.x + inc
                         && y >= pixel.y
                         && y < pixel.y + inc) {
                         pixel.color = this.activeColor;
-                        console.log(this.gridArr)
+                        // console.log(this.gridArr)
                         // console.log(this.activeColor)
                         break
                     };
